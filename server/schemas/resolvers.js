582a7df4bc9583d3f6b1cab439423fab808b9c7c
djteiji <djteiji@gmail.com>
2022-07-27 02:41:48 -0700

    invalid url error

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -57,12 +57,14 @@ const resolvers = {
       const order = new Order({ products: args.products });
       const { products } = await order.populate('products');
       const line_items = [];
+      const url = new URL(context.headers.referer).origin;
 
       for (let i = 0; i < products.length; i++) {
         // generate product id
         const product = await stripe.products.create({
           name: products[i].name,
-          description: products[i].description
+          description: products[i].description,
+          images: [`${url}/images/${products[i].image}`]
         });
 
         // generate price id using the product id
@@ -83,8 +85,8 @@ const resolvers = {
         payment_method_types: ['card'],
         line_items,
         mode: 'payment',
-        success_url: 'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
-        cancel_url: 'https://example.com/cancel'
+        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${url}/`
       });
       
       return { session: session.id };
